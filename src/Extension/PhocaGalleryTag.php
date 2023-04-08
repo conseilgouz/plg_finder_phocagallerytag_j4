31a2fb7373dcd6ac6f6bcfaa53935564c0e9d6f0
pmleconte <pascal.leconte@conseilgouz.com>
2023-04-08 14:40:56 +0200

    2.0.1
    Update tag finder on phoca gallery images updates

--- src/Extension/PhocaGalleryTag.php
+++ src/Extension/PhocaGalleryTag.php
@@ -30,7 +30,7 @@ final class PhocaGalleryTag extends Adapter
 	protected $table 			= '#__phocagallery';
 	protected $state_field      = 'published';
 	protected $autoloadLanguage = true;
-
+    protected $catid            = 0;
 
 	public function onFinderCategoryChangeState($extension, $pks, $value)
 	{
@@ -42,109 +42,89 @@ final class PhocaGalleryTag extends Adapter
 
 	public function onFinderAfterDelete($context, $table): void
 	{
-		if ($context == 'com_phocagallery.phocagallerytag')
-		{
+		if ( $context == 'com_phocagallery.phocagallerytag' || $context == 'com_phocagallery.phocagalleryimg' )	{
 			$id = $table->id;
 		}
-		elseif ($context == 'com_finder.index')
-		{
+		elseif ($context == 'com_finder.index')	{
 			$id = $table->link_id;
 		}
-		else
-		{
+		else {
 			return;
 		}
 		// Remove the items.
 		$this->remove($id);
 	}
-
 	public function onFinderAfterSave($context, $row, $isNew): void
 	{
 		// We only want to handle web links here. We need to handle front end and back end editing.
-		if ($context == 'com_phocagallery.phocagallerytag' || $context == 'com_phocagallery.tag' )
-		{
+		if ($context == 'com_phocagallery.phocagallerytag' || $context == 'com_phocagallery.tag' 
+		||  $context == 'com_phocagallery.phocagalleryimg' || $context == 'com_phocagallery.img' ) {
 			// Check if the access levels are different
-			if (!$isNew && $this->old_access != $row->access)
-			{
+			if (!$isNew && $this->old_access != $row->access) {
 				// Process the change.
 				$this->itemAccessChange($row);
 			}
-
 			// Reindex the item
 			$this->reindex($row->id);
 		}
 	}
-
-
 	public function onFinderBeforeSave($context, $row, $isNew)
 	{
 		// We only want to handle web links here
-		if ($context == 'com_phocagallery.phocagallerytag' || $context == 'com_phocagallery.tag' )
-		{
+		if ($context == 'com_phocagallery.phocagallerytag' || $context == 'com_phocagallery.tag' 
+		||  $context == 'com_phocagallery.phocagalleryimg' || $context == 'com_phocagallery.img' ) {
 			// Query the database for the old access level if the item isn't new
-			if (!$isNew)
-			{
+			if (!$isNew) {
 				$this->checkItemAccess($row);
 			}
 		}
-
 		// Check for access levels from the category
-		if ($context == 'com_phocagallery.phocagallerycat')
-		{
+		if ($context == 'com_phocagallery.phocagallerycat') {
 			// Query the database for the old access level if the item isn't new
-			if (!$isNew)
-			{
+			if (!$isNew) {
 				$this->checkCategoryAccess($row);
 			}
 		}
-
 		return true;
 	}
 
 	public function onFinderChangeState($context, $pks, $value)
 	{
 		// We only want to handle web links here
-		if ($context == 'com_phocagallery.phocagallerytag' || $context == 'com_phocagallery.tag' )
-		{
+		if ($context == 'com_phocagallery.phocagallerytag' || $context == 'com_phocagallery.tag' 
+		||	$context == 'com_phocagallery.phocagalleryimg' || $context == 'com_phocagallery.img' ) {
 			$this->itemStateChange($pks, $value);
 		}
 		// Handle when the plugin is disabled
-		if ($context == 'com_plugins.plugin' && $value === 0)
-		{
+		if ($context == 'com_plugins.plugin' && $value === 0) {
 			$this->pluginDisable($pks);
 		}
-
 	}
 
 
 	protected function index(Result $item)
 	{
 		// Check if the extension is enabled
-		if (ComponentHelper::isEnabled($this->extension) == false)
-		{
+		if (ComponentHelper::isEnabled($this->extension) == false) {
 			return;
 		}
-
 
         if (!ComponentHelper::isEnabled('com_phocagallery', true)) {
             echo '<div class="alert alert-danger">Phoca Gallery Error: Phoca Gallery component is not installed or not published on your system</div>';
             return;
         }
-
-		$item->setLanguage();
-
+        
+        $item->setLanguage();
 		// Initialize the item parameters.
 		$registry = new Registry;
 		$registry->loadString($item->params);
 		$item->params = $registry;
-
 		$registry = new Registry;
 		$registry->loadString($item->metadata);
 		$item->metadata = $registry;
-
+		$this->catid = $item->catid;
 		// Build the necessary route and path information.
-		$item->url = 'index.php?option=com_phocagallery&view=category&id='.$item->catid.'&tagid='.$item->id;
-
+		$item->url = $this->getURL($item->id,$this->extension, $this->layout);
 		$item->route = 'index.php?option=com_phocagallery&view=category&id='.$item->catid.'&tagid='.$item->id;
 
 		// Add the meta-author.
@@ -178,6 +158,7 @@ final class PhocaGalleryTag extends Adapter
 
 	protected function setup()
 	{
+//	    require_once JPATH_SITE . '/administrator/components/com_phocagallery/libraries/phocagallery/path/route.php';
 		return true;
 	}
 
@@ -205,4 +186,21 @@ final class PhocaGalleryTag extends Adapter
 			->where('a.published = 1');// todo
 		return $query;
 	}
+	protected function getStateQuery()
+	{
+		$query = $this->db->getQuery(true);
+		// Item ID
+		$query->select('a.id');
+		$query->select('a.published AS state, c.published AS cat_state');
+		// Item and category access levels
+		//$query->select(' a.access, c.access AS cat_access')
+		$query->select(' c.access AS cat_access')
+			->from($this->table . ' AS a')
+			->join('LEFT', '#__phocagallery_categories AS c ON c.id = a.link_cat');
+
+		return $query;
+	}
+	protected function getURL($id, $extension, $view) {
+		return 'index.php?option='.$extension.'&view='.$view.'&id='.$this->catid.'&tagid='.$id;
+	}		
 }
